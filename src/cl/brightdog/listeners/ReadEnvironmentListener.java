72335614b8428a4fe511378b45af1ffd0b7e36c7
ECD <ecofre@gmail.com>
2019-01-05 23:24:48 -0300

    Se agrega log4j

--- src/cl/brightdog/listeners/ReadEnvironmentListener.java
+++ src/cl/brightdog/listeners/ReadEnvironmentListener.java
@@ -4,26 +4,35 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.log4j.Logger;
+
 public class ReadEnvironmentListener implements ServletContextListener{
+	
+	final static Logger logger = Logger.getLogger(ReadEnvironmentListener.class);
 
 	  @Override
 	  public void contextDestroyed(ServletContextEvent arg0) {
+	
+		  logger.debug("[ReadEnvironmentListener][contextDestroyed]");
 		  
-	    //do stuff
 	  }
 
 	  @Override
 	  public void contextInitialized(ServletContextEvent context) {
 		  //do stuff before web application is started
-		  		  
-		  System.out.println("[ReadEnvironmentListener][contextInitialized] BOOKSTORE_API_NAME: " + System.getenv("BOOKSTORE_API_NAME"));
-		  System.out.println("[ReadEnvironmentListener][contextInitialized] BOOKSTORE_API_PORT: " + System.getenv("BOOKSTORE_API_PORT"));
-		  System.out.println("[ReadEnvironmentListener][contextInitialized] BOOKSTORE_API_HOST: " + System.getenv("BOOKSTORE_API_HOST"));
 		  
+		  logger.debug("[ReadEnvironmentListener][contextInitialized][START]");
+		  
+		  logger.debug("[ReadEnvironmentListener][contextInitialized][BOOKSTORE_API_NAME: " + System.getenv("BOOKSTORE_API_NAME") + "]");
+		  logger.debug("[ReadEnvironmentListener][contextInitialized][BOOKSTORE_API_PORT: " + System.getenv("BOOKSTORE_API_PORT") + "]");
+		  logger.debug("[ReadEnvironmentListener][contextInitialized][BOOKSTORE_API_HOST: " + System.getenv("BOOKSTORE_API_HOST") + "]");
+		   
 		  ServletContext servletContext = context.getServletContext();
 		  servletContext.setAttribute("BOOKSTORE_API_NAME", System.getenv("BOOKSTORE_API_NAME"));
 		  servletContext.setAttribute("BOOKSTORE_API_PORT", System.getenv("BOOKSTORE_API_PORT"));
 		  servletContext.setAttribute("BOOKSTORE_API_HOST", System.getenv("BOOKSTORE_API_HOST"));		  
+		  
+		  logger.debug("[ReadEnvironmentListener][contextInitialized][END]");
 		  
 	  }	
 	
